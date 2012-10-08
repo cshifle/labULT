07a5253f198f9e7cb3698a1c5f09d7b27249d7f8
Christopher Shifflett <cshiffle@rosemary.umw.edu>
2012-10-08 14:56:56 -0400

    added code to ULT.c

--- ULT.c
+++ ULT.c
@@ -22,8 +22,51 @@ ULT_CreateThread(void (*fn)(void *), void *parg)
 
 Tid ULT_Yield(Tid wantTid)
 {
-  assert(0); /* TBD */
-  return ULT_FAILED;
+
+  volatile Tid ret;
+  if(wantTid < -2 || wantTid >= ULT_MAX_THREADS) {
+    return ULT_INVALID;
+  }
+  // store current thread
+  ThrdCtlBlk * current_thread = (ThrdCtlBlk*)malloc(sizeof(ThrdCtlBlk));
+  current_thread->rfgc = false;
+  current_thread->tid  = current_tid;
+  getcontext(&(current_thread->context));
+  if(!current_thread->rfgc) {
+    current_thread->rfgc = true;
+    ready_list.push_back(current_thread);
+
+    // pick the next tcb to run
+    ThrdCtlBlk * tcb_to_run;
+    if(wantTid == ULT_ANY) {
+      tcb_to_run = ready_list.front();
+      ready_list.pop_front();
+    }
+    else if(wantTid == ULT_SELF) {
+      tcb_to_run = ready_list.back();
+      ready_list.pop_back();
+    }
+    else {
+      tcb_to_run = findTcb(wantTid);
+      if(!tcb_to_run) {
+        ready_list.pop_back();
+        return ULT_INVALID;
+      }
+    }
+    current_tid = tcb_to_run->tid;
+    ret = current_tid;
+    setcontext(&(tcb_to_run->context));
+  }
+
+  if(ready_list.empty()) {
+    if(wantTid == ULT_ANY) {
+      ret = ULT_NONE;
+    }
+  }
+  free(current_thread);
+  return ret;
+ // assert(0); /* TBD */
+ // return ULT_FAILED;
 
 }
 
